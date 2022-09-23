ac1e721477189d9da907f0bba938398cb688b1a0
Victor Bello <you@example.com>
2022-09-23 17:11:38 +0100

    dates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react"
 import Calendar from 'react-calendar'
 
+import 'react-calendar/dist/Calendar.css';
+
 
 
 function App() {
 
-//   const disableDates = [
-//     new Date('August 19, 2022 23:15:30'), // Jan, 2nd
-//     new Date('August 20, 2022 23:15:30'), // Aug, 7th
-//  ]
-  const disableDates = new Date('August 19, 2022 23:15:30');
-  const date1=disableDates.getDate();
+  const disableDates = [
+    new Date('August 19, 2022 23:15:30'), // Jan, 2nd
+    new Date('August 20, 2022 23:15:30'),
+    new Date('August 21, 2022 23:15:30'),
+    new Date('August 22, 2022 23:15:30'), // Aug, 7th
+ ]
+  // const disableDates = new Date('August 20, 2022 23:15:30');
+  // const date1=disableDates.getDate();
 return(
 <div className="calendar">
 <Calendar
- tileDisabled={({date}) => date.getDate()===date1}
+ tileDisabled={({date}) => {
+  
+  return disableDates.find(element => date.getDate() === element.getDate() );}}
 />
 </div>
 )
